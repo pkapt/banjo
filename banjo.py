1053415bff2c21b19ef9ed9a2f2dbc952b2743a9
Peter Kapteyn <peter.kapteyn@dornerworks.com>
2020-08-15 21:48:43 -0400

    Get freq of generated sine wave

--- banjo.py
+++ banjo.py
@@ -14,17 +14,10 @@ y1 = np.sin(2*np.pi*f * (x/samples))
 def fft_plot(audio, sampling_rate):
     n = len(audio)
     T = 1/sampling_rate
-    yf = scipy.fft(audio)
-    # yf = 2.0/n * np.abs(yf[:n//2])
+    yf = scipy.fft.fft(audio)
+    yf = 2.0/n * np.abs(yf[:n//2])
     xf = np.linspace(int(0), int(1.0/(2.0*T)), int(n/2))
-    print(xf)
-    fig, ax = plt.subplots()
-    ax.plot(xf, 2.0/n * np.abs(yf[:n//2]))
-    plt.grid()
-    plt.xlabel("freq")
-    plt.ylabel("mag")
-    return plt.show()
-    return yf
+    return xf[np.where(yf == np.amax(yf))]
 
 yf = fft_plot(y1, samples)
 print(yf)
