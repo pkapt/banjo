09b5a439edd69e2a28a0740027cb3a028079b370
Peter Kapteyn <peter.kapteyn@dornerworks.com>
2020-08-17 22:49:53 -0400

    Janky working on test cmaj

--- banjo.py
+++ banjo.py
@@ -2,14 +2,12 @@ import numpy as np
 import scipy
 from scipy.io import wavfile
 from pitches import Converter
+from pytablature.tab import Tab, Note
+from pytablature.tab.const import *
+from math import floor
 
-file_path = "guitar.wav"
+file_path = "cmaj.wav"
 SAMPLE_FREQ = 26 #samples per sec
-
-samples = 100
-f = 3
-x = np.arange(samples)
-y1 = np.sin(2*np.pi*f * (x/samples))
 
 def get_freq(audio, sampling_rate):
     n = len(audio)
@@ -19,26 +17,43 @@ def get_freq(audio, sampling_rate):
     xf = np.linspace(int(0), int(1.0/(2.0*T)), int(n/2))
     return xf[np.where(yf == np.amax(yf))]
 
-def stupid_get_closest_divisor(length, base_div):
-    while True:
-        if length % base_div == 0:
-            return base_div
-        base_div += 1
-
+'''
+split the audio file into a bunch of chunks
+'''
 (rate, sig) = wavfile.read(file_path)
 duration = len(sig) / rate # samples per sec
-split_num = round(len(sig) / 1000)
-print(split_num)
+split_num = floor(len(sig) / 1000)
 sig = sig[:len(sig)-len(sig)%1000]
-print(len(sig))
 sig = np.split(sig, split_num) # SPLIT AUDIO FILE INTO SMALL CHUNKS
 
+'''
+get the strongest frequency associated with each chunk 
+and convert the frequencies to notes
+'''
 c = Converter()
-print("got here")
+note_chunks = []
 for chunk in sig:
     data = chunk[:,0]
     freq = get_freq(data, rate)
-    print(c.pitch(freq[0]))
+    if floor(freq[0]) != 0:
+        note_chunks.append(c.pitch(freq[0]))
 
+'''
+group all the similar consecutive chunks into notes
+'''
+notes = []
+last_chunk = note_chunks[0]
+for current_chunk in note_chunks:
+    if current_chunk != last_chunk:
+        notes.append(last_chunk)
+    last_chunk = current_chunk
+
+'''
+convert the notes into a tab
+'''
+notes = [Note(note, duration=QUARTER) for note in notes]
+
+tab = Tab(notes)
+tab.generate()
 
 
---
+++ pytablature/README.md
@@ -0,0 +1,7 @@
+# pytablature
+
+python library to create/edit tabs for guitar
+
+## install dependancies
+
+pip install -r req.txt
